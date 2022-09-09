36f774e7408386dbe613b37b5e20d2bf83303016
Agustin Lopez <agusdlopez21@gmail.com>
2022-09-09 00:29:23 -0300

    Proyecto Final de JavaSccript

--- javascript/eventos.js
+++ javascript/eventos.js
@@ -1,3 +1,4 @@
+let cantCarrito = capturarStorage2()  
 
 function capturarStorage(){
     return JSON.parse(localStorage.getItem("carrito")) || []
@@ -7,7 +8,16 @@ function guardarStorage(array){
     localStorage.setItem("carrito", JSON.stringify(array))
 }
 
-function agregarAlCarrito(id) {
+function capturarStorage2(){
+    return JSON.parse(localStorage.getItem("cantCarrito")) || 0
+}
+
+function guardarStorage2(cantCarrito){
+    localStorage.setItem("cantCarrito", JSON.stringify(cantCarrito))
+}
+
+
+function agregarAlCarrito(id) { // funcion para agregar productos al carrito
     let carrito = capturarStorage()
     if (estaEnCarrito(id)){
         incrementarCant(id)
@@ -16,13 +26,25 @@ function agregarAlCarrito(id) {
         carrito.push({...productoEncontrado, cantidad: 1})
         guardarStorage(carrito)
         mostrarCarrito()
+        Toastify({
+            text: "Producto agregado al carrito",
+            duration: 3000,
+            newWindow: true,
+            close: true,
+            gravity: "top", 
+            position: "right", 
+            stopOnFocus: true,
+            style: {
+                background: "linear-gradient(to right, #00b09b, #96c93d)",
+            } // Callback after click
+        }).showToast();
     }
     mostrarCarrito()
     console.log(carrito)
     mostrarTotalCarrito()
 }
 
-function incrementarCant(id) {
+function incrementarCant(id) {  // Funcion para incrementar cantidad cada vez que se hace clic en el boton +
     let carrito = capturarStorage()
     const indice = carrito.findIndex(juego => juego.id==id)
     carrito[indice].cantidad++
@@ -32,12 +54,13 @@ function incrementarCant(id) {
 }
 
 
-function restarCant(id) {
+function restarCant(id) { //Funcion para restar cantidad cada vez que se presiona el boton -
     let carrito = capturarStorage();
     const index = carrito.findIndex((e) => e.id == id); // busco la posicion del objeto
     if (carrito[index].cantidad > 1) {
       carrito[index].cantidad--; //segun la posicion le resto uno a cantidad
       guardarStorage(carrito);
+      restarEnCarrito();
       mostrarCarrito();
       mostrarTotalCarrito();
     } else {
@@ -58,23 +81,34 @@ function restarCant(id) {
             )}
         }
 
-function estaEnCarrito(id){
+function estaEnCarrito(id){ // verificacion de si esta en el carrito o no
     let carrito = capturarStorage()
     return carrito.some(e=>e.id==id)
 }
 
-function eliminarProductoCarrito(id) {
-    let carrito = capturarStorage()
-    //filtramos y hacemos que nos devuelva todo menos el id buscado
-    let resultado = carrito.filter(juego => juego.id != id)
+function eliminarProductoCarrito(id) {// funcion para eliminar producto del carrito se filtra y hace que nos devuelva todo menos el id buscado
+    /* let carrito = capturarStorage()
+    let resultado = carrito.filter(juego => juego.id != id)    
     guardarStorage(resultado)
     console.log(resultado)
     mostrarCarrito()
+    mostrarTotalCarrito() */
+    let carrito = capturarStorage()
+    let resultado = carrito.filter(celular => celular.id != id)
+    const index = carrito.findIndex((e) => e.id == id);
+    cantCarrito -= carrito[index].cantidad
+    guardarStorage(resultado)
+    guardarStorage2(cantCarrito)
+    mostrarEnCarrito()
+    console.log(resultado)
+    mostrarCarrito()
     mostrarTotalCarrito()
+    
+    borrarCarrito()
 }
 
 
-botonPedidoRealizado.addEventListener('click', () => {
+botonPedidoRealizado.addEventListener('click', () => { // listener para realizar el pedido y borrar el carrito
     if (estaVacio()) {
         Swal.fire({
             icon: 'error',
@@ -90,6 +124,7 @@ botonPedidoRealizado.addEventListener('click', () => {
         localStorage.clear();
         mostrarCarrito();
         mostrarTotalCarrito();
+        mostrarEnCarrito()
     }
 })
 
@@ -100,3 +135,55 @@ function estaVacio() {
         return false
     }
 }
+
+
+ // ----------- FUNCIONES PARA MODIFICAR EL NUMERO QUE SE ENCUENTRA AL LADO DEL CARRITO --------
+ function aumentarEnCarrito() {
+    cantCarrito++
+    mostrarEnCarrito()
+    guardarStorage2(cantCarrito)
+}
+
+function restarEnCarrito(){
+    cantCarrito--
+    mostrarEnCarrito()
+    guardarStorage2(cantCarrito)
+}
+
+function borrarCarrito(){
+    cantCarrito = 0
+}
+
+function mostrarEnCarrito(){
+    cartAmount.innerHTML=""
+    cartAmount.innerHTML=`${cantCarrito}`
+}
+
+// -------------------------------------------------------------------------------------------------
+
+const busqueda = () =>{ //funcion buscador
+    const texto = formulario.value.toLowerCase();
+    let encontro = false
+    divCelulares.innerHTML=""
+    for (let celular of dataTelefonos) {
+        let descripcion = celular.descripcion.toLowerCase();
+        if (descripcion.indexOf(texto) !== -1){
+            encontro = true
+            divCelulares.innerHTML+=`
+            <div class="card mb-4" style="width: 18rem;">
+                <img src="${celular.img}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${celular.descripcion}</h5>
+                    <p class="card-text">$ ${celular.precio}</p>
+                    <button onclick="agregarAlCarrito(${celular.id})" id="botonAgregar" class="btn btn-success">Agregar al Carrito</button>
+                </div>
+            </div>
+            `
+        }   
+    }
+    if (encontro == false) {
+        divCelulares.innerHTML=""
+        divCelulares.innerHTML="<p>Producto no encontrado</p>"
+    }  
+}
+botonBuscar.addEventListener('click', busqueda)
--- javascript/main.js
+++ javascript/main.js
@@ -1,6 +1,10 @@
 const divGames = document.getElementById("divGames")
 const tablaCarrito = document.getElementById("tablaCarrito")
 const totalCarrito = document.getElementById("totalCarrito")
+const botonAgregar = document.getElementById("botonAgregar")
+const formulario = document.getElementById("formulario")
+const botonBuscar = document.getElementById("botonBuscar")
+let cartAmount = document.getElementById("cartAmount")
 let dataJuegos
 
 document.addEventListener('DOMContentLoaded', () => { //Cargamos funciones con DomContent
@@ -41,13 +45,12 @@ function mostrarCarrito() { // Mostramos carrito
         <tr">
             <td data-th="Product">
                 <div class="row">
-                <div class="col-sm-2 hidden-xs"><img src="${element.img}" width=64px alt="..."/></div>
+                <div class="col-sm-2 hidden-xs"><img src="${element.img}" width=60px alt="..."/></div>
                     <div class="col-sm-10">
-                        <h4 class="nomargin">${element.titulo} ${element.juego}</h4>
+                        <h5 class="nomargin">${element.titulo} ${element.juego}</h5>
                     </div>
                 </div>
             </td>
-            <td data-th="Precio">${element.precio}</td>
             <td data-th="Cantidad">
                 <div class="column d-flex align-items-center">
                     <button class="btn btn-sm" onclick="restarCant(${element.id})"><i class="fa-solid fa-square-minus"></i></button>
@@ -56,13 +59,13 @@ function mostrarCarrito() { // Mostramos carrito
                 </div>
             </td>
             <td data-th="Subtotal" class="text-center">${element.precio * element.cantidad}</td>
-            <td><button onclick="eliminarProductoCarrito(${element.id})" class="btn btn-danger btn-sm"><i class="fa-solid fa-trash-can"></i></button></td>
+            <td><button onclick="restarCant(${element.id})" class="btn btn-danger btn-sm"><i class="fa-solid fa-trash-can"></i></button></td>
         </tr>
         `
     });
 }
 
-function mostrarTotalCarrito() {
+function mostrarTotalCarrito() { //Funcion para mostrar el total del carrito
     //calculo el valor total
     const carrito = capturarStorage();
     const total = carrito.reduce(
